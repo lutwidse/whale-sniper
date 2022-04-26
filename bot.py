12abfba7cb05a6ebacf56a1f7cd7ae8ff88ccbe0
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-04-27 04:00:53 +0900

    add liquidation alert, fix some dirty code

--- bot.py
+++ bot.py
@@ -4,29 +4,49 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 import schedule
 import time
 import os
+import csv
 
 ws = WhaleSniper()
-def update_transactions():
+def update_bids():
     ws.get_transactions()
     ws.get_bids()
     ws.extract_bids()
-    ws.__init()
+
+def update_liquidations():
+    ws.get_liquidations()
+    ws.extract_liquidations()
 
 HELP_TEXT = (
     "This bot is still in development. alert might be reset for some reason. \n"
     "/whale - Get latest whales bid. \n"
-    "/alert - Set /whale notification every 1 hour. \n"
-    "/unalert - Unset alert."
+    "/whale_alert - Set /whale notification every 1 hour. \n"
+    "/whale_unalert - Unset alert. \n\n"
+
+    "/liquidation - Get LUNA liquidation data\n" 
+    "/liquidation_alert - Set /liquidation notification every 1 hour. \n"
+    "/liquidation_unalert - Unset alert."
 )
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(text="Hi! Please use /help to see commands.")
+def get_extracted_bids(cnt):
+        with open("whale_sniper/luna_whale.csv", "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            b = ""
+            for i, j in enumerate(reader):
+                b += (
+                      f"{j['timestamp']} - {j['sender']} \n"
+                    + f"[Amount] {j['amount']} [Premium] {j['premium_slot']} [BLT] {j['strategy_activate_ltv']} [ARCT] {j['strategy_activate_amount']} \n\n")
+                if i == cnt-1:
+                    return b
 
-def help(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(text=HELP_TEXT)
-
-def whale(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(ws.get_extracted_bids())
+def get_extracted_liquidation(cnt):
+        with open("whale_sniper/luna_liquidation.csv", "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            b = ""
+            for i, j in enumerate(reader):
+                b += (
+                    f"[Liquidation Price] ${j['Luna_Liquidation_Price']} [Amount] {j['Loan_Value']}\n\n")
+                if i == cnt-1:
+                    return b
 
 def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     current_jobs = context.job_queue.get_jobs_by_name(name)
@@ -36,14 +56,23 @@ def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
         job.schedule_removal()
     return True
 
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(text="Hi! Please use /help to see commands.")
+
+def help(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(text=HELP_TEXT)
+
+def whale(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(get_extracted_bids(5))
+
 def whale_alert(context: CallbackContext) -> None:
     job = context.job
-    context.bot.send_message(job.context, text=ws.get_extracted_bids())
+    context.bot.send_message(job.context, text=get_extracted_bids(5))
 
 def set_whale_alert(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_repeating(whale_alert, interval=60*60, context=chat_id, name=str(chat_id))
+    context.job_queue.run_repeating(whale_alert, interval=60*60, context=chat_id, name=str(chat_id)+"_whale")
 
     if job_removed:
         update.message.reply_text("Alert has been reset.")
@@ -51,6 +80,29 @@ def set_whale_alert(update: Update, context: CallbackContext) -> None:
         update.message.reply_text("Alert has been set.")
 
 def unset_whale_alert(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id) + "_whale", context)
+    text = "Unalerted   " if job_removed else 'You have no active alert.'
+    update.message.reply_text(text)
+
+def liquidation(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(get_extracted_liquidation(5))
+
+def liquidation_alert(context: CallbackContext) -> None:
+    job = context.job
+    context.bot.send_message(job.context, text=get_extracted_liquidation(5))
+
+def set_liquidation_alert(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id) + "_liquidation", context)
+    context.job_queue.run_repeating(liquidation_alert, interval=60*60, context=chat_id, name=str(chat_id)+"_liquidation")
+
+    if job_removed:
+        update.message.reply_text("Alert has been reset.")
+    else:
+        update.message.reply_text("Alert has been set.")
+
+def unset_liquidation_alert(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
     text = "Unalerted   " if job_removed else 'You have no active alert.'
@@ -60,15 +112,23 @@ if __name__ == "__main__":
     updater = Updater(os.environ["TOKEN"])
     updater.dispatcher.add_handler(CommandHandler("start", start))
     updater.dispatcher.add_handler(CommandHandler("help", help))
+
     updater.dispatcher.add_handler(CommandHandler("whale", whale))
-    updater.dispatcher.add_handler(CommandHandler("alert", set_whale_alert))
-    updater.dispatcher.add_handler(CommandHandler("unalert", unset_whale_alert))
+    updater.dispatcher.add_handler(CommandHandler("whale_alert", set_whale_alert))
+    updater.dispatcher.add_handler(CommandHandler("whale_unalert", unset_whale_alert))
+
+    updater.dispatcher.add_handler(CommandHandler("liquidation", liquidation))
+    updater.dispatcher.add_handler(CommandHandler("liquidation_alert", set_liquidation_alert))
+    updater.dispatcher.add_handler(CommandHandler("liquidation_unalert", unset_liquidation_alert))
+
     updater.start_polling()
     
     # init
-    update_transactions()
+    update_bids()
+    update_liquidations()
 
-    schedule.every(1).hours.do(update_transactions)
+    schedule.every(1).hours.do(update_bids)
+    schedule.every(30).minutes.do(update_liquidations)
     while True:
         schedule.run_pending()
         time.sleep(1)
--- whale_sniper/whale_sniper.py
+++ whale_sniper/whale_sniper.py
@@ -1,31 +1,35 @@
 import requests
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta
 import csv
-#LOCAL_TIMEZONE = datetime.utcnow().astimezone().tzinfo
 
-API = "https://fcd.terra.dev/v1/txs?limit=100&account="
+TERRA_API = "https://fcd.terra.dev/"
+TXS_API = TERRA_API + "v1/txs?limit=100&account="
+
+ALPHA_DEFI_API = "https://api.alphadefi.fund/"
+LIQUIDATION_API = ALPHA_DEFI_API + "historical/kujira/profiles/?datetime="
+
 KUJIRA_ORCA_AUST_VAULT = "terra13nk2cjepdzzwfqy740pxzpe3x75pd6g0grxm2z"
 BLUNA = "terra1kc87mu460fwkqte29rquh4hc20m54fxwtsx7gp"
-
 class WhaleSniper:
     def __init__(self):
-        self.transactions = []
-        self.bids = []
+        self._transactions = []
+        self._bids = []
+        self._liquidations = []
 
-    def init(self):
-        self.transactions = []
-        self.bids = []
-
+    def _get_ts(self) -> datetime:
+        return datetime.utcnow()
+    
     def get_transactions(self):
+        self._transactions.clear()
         with open("whale_sniper/whale.txt", "r") as f:
             for w in f.readlines():
                 address = w.strip("\n")
-                res = requests.get(f"{API}{address}")
-                self.transactions.append(res.json())
+                res = requests.get(f"{TXS_API}{address}")
+                self._transactions.append(res.json())
     
     def get_bids(self):
-        for transaction in self.transactions:
+        self._bids.clear()
+        for transaction in self._transactions:
             for txs in transaction["txs"]:
                 if len(txs["logs"]) == 0:
                     continue
@@ -50,20 +54,21 @@ class WhaleSniper:
                 
                 collateral_token = wasm[10]["value"]
                 if collateral_token == BLUNA:
-                    timestamp = txs["timestamp"]
-                    utc_timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=ZoneInfo("US/Central"))
-                    #local_timestamp = utc_timestamp.astimezone(LOCAL_TIMEZONE)
+                    timestamp = datetime.strptime(txs["timestamp"], "%Y-%m-%dT%H:%M:%SZ").isoformat(" ")
 
                     amount = wasm[8]["value"]
                     premium_slot = wasm[9]["value"]
                     strategy_activate_ltv = wasm[12]["value"]
                     strategy_activate_amount = wasm[13]["value"]
                     amount = amount[:-6] if len(amount) > 6 else amount[:-5]
-                    self.bids.append([utc_timestamp.isoformat(" "), f"{sender_contract[6:14]}", f"{'{:.2f}'.format((int(amount)/100000))}M", f"{premium_slot}%", f"{strategy_activate_ltv}%", f"{int(strategy_activate_amount)/1000000000000}M"])
+
+                    rounted_num = round(int(amount))
+                    rounted_num = f"{(rounted_num/1000):.1f}K" if (1000000 > rounted_num > 1000) else f"{(rounted_num/1000000):.1f}M" if rounted_num >= 1000000 else rounted_num
+                    self._bids.append([timestamp, f"{sender_contract[6:14]}", rounted_num, f"{premium_slot}%", f"{strategy_activate_ltv}%", f"{int(strategy_activate_amount)/1000000000000}M"])
 
     def extract_bids(self):
-        sorted_bids = sorted(self.bids, key=lambda t: datetime.strptime(t[0], "%Y-%m-%d %H:%M:%S%z"), reverse=True)
-        with open("whale_sniper/whale_bot.csv", "w") as csvfile:
+        sorted_bids = sorted(self._bids, key=lambda t: datetime.strptime(t[0], "%Y-%m-%d %H:%M:%S"), reverse=True)
+        with open("whale_sniper/luna_whale.csv", "w") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=["timestamp", "sender", "amount", "premium_slot", "strategy_activate_ltv", "strategy_activate_amount"])
             writer.writeheader()
             for i in sorted_bids:
@@ -75,14 +80,23 @@ class WhaleSniper:
                         "strategy_activate_ltv": i[4],
                         "strategy_activate_amount": i[5]
                         })
-    
-    def get_extracted_bids(self):
-        with open("whale_sniper/whale_bot.csv", "r") as csvfile:
-            reader = csv.DictReader(csvfile)
-            b = ""
-            for i, j in enumerate(reader):
-                b += (
-                      f"{j['timestamp']} - {j['sender']} \n"
-                    + f"[Amount] {j['amount']} [Premium] {j['premium_slot']} [BLT] {j['strategy_activate_ltv']} [ARCT] {j['strategy_activate_amount']} \n\n")
-                if i == 4:
-                    return b
+
+    def get_liquidations(self):
+        self._liquidations.clear()
+        timestamp = (self._get_ts() - timedelta(hours=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        res = requests.get(f"{LIQUIDATION_API}{timestamp}")
+        for i in res.json():
+            self._liquidations.append(i)
+
+    def extract_liquidations(self):
+        sorted_liquidations = sorted(self._liquidations, key=lambda t: t["Luna_Liquidation_Price"], reverse=True)
+        with open("whale_sniper/luna_liquidation.csv", "w") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["Luna_Liquidation_Price", "Loan_Value"])
+            writer.writeheader()
+            for i in sorted_liquidations:
+                rounted_num = round(int(i["Loan_Value"]))
+                rounted_num = f"{(rounted_num/1000):.1f}K" if (1000000 > rounted_num > 1000) else f"{(rounted_num/1000000):.1f}M" if rounted_num >= 1000000 else rounted_num
+                writer.writerow({
+                        "Luna_Liquidation_Price": i["Luna_Liquidation_Price"],
+                        "Loan_Value": rounted_num
+                        })
